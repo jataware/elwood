b191fbfeda457f23cecc21b4a28077f23bc7c867
Powell <powellfendley@gmail.com>
2023-03-23 11:59:58 -0600

    Updated temporal scaling transformation to take an optional geo_columns list that will prevent lat/lon and other geo information from being modified by the rescaling process.

--- elwood/transformations/scaling.py
+++ elwood/transformations/scaling.py
@@ -1,7 +1,9 @@
 import pandas
 
 
-def scale_time(dataframe, time_column, time_bucket, aggregation_function_list):
+def scale_time(
+    dataframe, time_column, time_bucket, aggregation_function_list, geo_columns=None
+):
     """Scales timestamp data in a dataframe to a less granular time frequency
 
     Args:
@@ -9,9 +11,15 @@ def scale_time(dataframe, time_column, time_bucket, aggregation_function_list):
         time_column (string): Name of the time column to scale in the dataframe.
         time_bucket (DateOffset, Timedelta or str): The offset string or object representing target conversion. ex. "2H", "M"
         aggregation_function (List[string]): A list of aggregation functions like sum, average, median, mean, mode, etc. Example: ['sum'], or ['min', 'max', 'sum']
+        geo_columns (List[string]): A list of the geographical columns that should not be modified by the transformation.
     """
 
     dataframe[time_column] = pandas.to_datetime(dataframe[time_column])
+
+    if geo_columns:
+        dataframe = dataframe.set_index(geo_columns)
+
+        dataframe = dataframe.groupby(geo_columns)
 
     # dataframe.set_index(time_column).resample(time_bucket).agg(
     # aggregation_function_list
@@ -19,5 +27,8 @@ def scale_time(dataframe, time_column, time_bucket, aggregation_function_list):
     scaled_frame = dataframe.resample(time_bucket, on=time_column).agg(
         aggregation_function_list[0]
     )
+
+    # scaled_frame.columns = scaled_frame.columns.get_level_values(0)
+    scaled_frame.reset_index(inplace=True)
 
     return scaled_frame
