ab5841cb0e863194bd6791ea9097792e6df69320
Adam Gilgoff <adam.gilg@gmail.com>
2023-07-12 16:39:05 -0400

    Changes scale_time to take geo_columns as dict, adds aggregation function list error handling

--- elwood/transformations/scaling.py
+++ elwood/transformations/scaling.py
@@ -12,22 +12,26 @@ def scale_time(
         time_bucket (DateOffset, Timedelta or str): The offset string or object representing target conversion. ex. "2H", "M"
         aggregation_functions (List[string] or Dict{str:str}): A list containing one aggregation function, or a Dict containing column
             names and aggregation functions like sum, average, median, mean, mode, etc. Example: ['sum'], or {'temp': 'min', 'rainfall': 'max', 'visitors': 'sum'}
-        geo_columns (List[string]): A list of the geographical columns that should not be modified by the transformation.
+        geo_columns (dict): A dict containing the column names for the lat/lon columns in the dataframe, with keys 'lat' and 'lon'.
     """
 
     dataframe[time_column] = pandas.to_datetime(dataframe[time_column])
 
-    if geo_columns:
-        dataframe = dataframe.set_index(geo_columns)
+    if geo_columns and 'lat_column' in geo_columns and 'lon_column' in geo_columns:
+        geo_columns_list = [geo_columns['lat_column'], geo_columns['lon_column']]  # Extracts the values (column names) from the dict
+        dataframe = dataframe.set_index(geo_columns_list)
+        dataframe = dataframe.groupby(geo_columns_list)
 
-        dataframe = dataframe.groupby(geo_columns)
+    if aggregation_functions and all(aggregation_functions):
+        aggregator = aggregation_functions
+        if isinstance(aggregation_functions, list):
+            aggregator = aggregation_functions[0]
+        scaled_frame = dataframe.resample(time_bucket, on=time_column).agg(aggregator)
 
-    aggregator = aggregation_functions
-    if isinstance(aggregation_functions, list):
-        aggregator = aggregation_functions[0]
-    scaled_frame = dataframe.resample(time_bucket, on=time_column).agg(aggregator)
 
-    # scaled_frame.columns = scaled_frame.columns.get_level_values(0)
-    scaled_frame.reset_index(inplace=True)
+        # scaled_frame.columns = scaled_frame.columns.get_level_values(0)
+        scaled_frame.reset_index(inplace=True)
 
-    return scaled_frame
+        return scaled_frame
+    else:
+        raise ValueError("Invalid aggregation functions provided.")
